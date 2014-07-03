947a622b63094614307354bf2cdae7c5a23f0c06
drammopo <drammopo@gmail.com>
2014-07-03 22:20:13 +0200

    added normal functionality

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -57,12 +57,10 @@ class GildedRose
   end
 
   def normal_tick
-    if @days_remaining > 0
+      @days_remaining -= 1
+      return if @quality == 0
+
       @quality -= 1
-    end
-    if @days_remaining <= 0
-      @quality -= 2
-    end
-    @days_remaining -= 1
+      @quality -= 1 if @days_remaining <= 0
   end
 end
